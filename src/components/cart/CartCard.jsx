1e5ad2031fd4968bac3aa65fccf765f3ffc5fa66
Opeyemi Omotayo <96225514+Opeyemi-Omotayo@users.noreply.github.com>
2024-10-13 19:49:23 +0100

    feat: get cart functionality

--- src/components/cart/CartCard.jsx
+++ src/components/cart/CartCard.jsx
@@ -1,36 +1,49 @@
 import React from "react";
 import { MdDeleteOutline } from "react-icons/md";
+import { useAppSelector } from "../../Store/hooks";
 
 const CartCard = () => {
+  const { carts } = useAppSelector((state) => state.carts);
+
   return (
     <div className="flex flex-col items-start bg-white shadow-md rounded-md w-full ">
-      <h1 className="font-semibold border-b w-full p-3 text-base">Cart(0)</h1>
-      <div className="flex items-start flex-col px-3 py-4 w-full">
-        <div className="flex items-center justify-between w-full">
-          <div className="flex items-start">
-            <img
-              src="images/shoes.png"
-              alt="shoes"
-              className="w-20 h-20 rounded-md shadow mr-4"
-            />
-            <div>
-              <h1 className="font-medium text-lg text-gray-600 leading-5">
-                name
+      <h1 className="font-semibold border-b w-full p-3 text-base">
+        Cart({carts?.length})
+      </h1>
+      {carts.map((cart) => (
+        <div className="flex items-start flex-col px-3 py-4 w-full border-t border-gray-100" key={cart.id}>
+          <div className="flex items-center justify-between w-full">
+            <div className="flex items-start">
+              <img
+                src={cart?.images[0]}
+                alt={cart?.title}
+                className="w-20 h-20 rounded-md shadow mr-4"
+              />
+              <div>
+                <h1 className="font-medium text-lg text-gray-600 leading-5">
+                  {cart?.title}
+                </h1>
+                <p className="text-primary text-xs">stock: <span className="text-gray-700">{cart?.availabilityStatus}</span></p>
+              </div>
+            </div>
+            <h1 className="font-semibold text-lg">₦{cart?.price}</h1>
+          </div>
+          <div className="pt-2 flex items-center justify-between w-full">
+            <h1 className="text-primary uppercase flex items-center">
+              <MdDeleteOutline className="mr-3 w-6 h-6" /> Remove
+            </h1>
+            <div className="flex items-center">
+              <h1 className="bg-primary rounded-sm hover:bg-primary100 px-3 py-1 mr-2 cursor-pointer text-white">
+                -
               </h1>
-              <p className="text-primary text-xs">stock</p>
+              <h1 className=" px-3 py-1 mr-2 cursor-pointer ">0</h1>
+              <h1 className="bg-primary cursor-pointer hover:bg-primary100 rounded-sm px-3 py-1 text-white">
+                +
+              </h1>
             </div>
           </div>
-          <h1 className="font-semibold text-lg">price</h1>
         </div>
-        <div className="pt-2 flex items-center justify-between w-full">
-            <h1 className="text-primary uppercase flex items-center"><MdDeleteOutline className="mr-3 w-6 h-6" />  Remove</h1>
-            <div className="flex items-center">
-                <h1 className="bg-primary rounded-sm hover:bg-primary100 px-3 py-1 mr-2 cursor-pointer text-white">-</h1>
-                <h1 className=" px-3 py-1 mr-2 cursor-pointer ">0</h1>
-                <h1 className="bg-primary cursor-pointer hover:bg-primary100 rounded-sm px-3 py-1 text-white">+</h1>
-            </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
--- src/components/cart/CartSummary.jsx
+++ src/components/cart/CartSummary.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { useAppSelector } from "../../Store/hooks";
 
 const CartSummary = () => {
-  return (
+  const {carts} = useAppSelector((state) => state.carts);
+
+  const calculateTotalPrice = carts.reduce((total, cart) => {
+    return total + cart.price * cart.quantity;
+  }, 0);
+
+    return (
     <div className="flex flex-col items-start text-sm bg-white shadow-md rounded-md w-full">
       <h1 className="font-semibold uppercase border-b w-full p-3 text-base">
         Cart summary
@@ -9,11 +16,11 @@ const CartSummary = () => {
       <div className=" w-full">
         <div className="flex items-center justify-between border-b py-3">
           <p className="text-sm text-gray-500 px-3">Subtotal</p>
-          <h2 className="text-lg font-semibold px-3">N56788</h2>
+          <h2 className="text-lg font-semibold px-3">₦{calculateTotalPrice}</h2>
         </div>
         <div className="p-3">
         <button className="bg-primary text-white rounded-md uppercase shadow-lg w-full text-center py-3 flex items-center justify-center relative">
-          checkout (N 56677)
+          checkout (₦ {calculateTotalPrice})
         </button>
         </div>
       </div>
